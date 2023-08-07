2a93d00f1f122c01102c62f8bb557f33bc969d6d
Saif Ali <saifsolangi2@gmail.com>
2023-08-07 13:50:28 +0500

    fibonacci series program added

--- logicBuildingPractice.js
+++ logicBuildingPractice.js
@@ -39,3 +39,34 @@ console.log(findFactorial(5))
 
 
 
+// fibonacci Series
+function fibonacciNumbers(n) {
+
+    // Initialize variables
+    let previous = 0;
+    let current = 1;
+    let count = 2;
+
+    // Print the first two Fibonacci numbers
+    console.log(previous);
+    console.log(current);
+
+    // Loop to generate Fibonacci series
+    while (count < n) {
+        // Calculate the next Fibonacci number
+        const next = previous + current;
+
+        // Print the generated Fibonacci number
+        console.log(next);
+
+        // Update variables for the next iteration
+        previous = current;
+        current = next;
+        count++;
+    }
+
+}
+
+fibonacciNumbers(7)
+
+
