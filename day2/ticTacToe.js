0e2b78c075676de589a5c6a9ccd820d69da79614
Saif Ali <saifsolangi2@gmail.com>
2023-08-09 12:39:50 +0500

    made some changes to set rounds

--- day2/ticTacToe.js
+++ day2/ticTacToe.js
@@ -1,10 +1,25 @@
 const gameboard = document.querySelector("#gameboard")
 const infoDisplay = document.querySelector("#info")
+const winner = document.querySelector("#winner")
 
 const startCells = ["", "", "", "", "", "", "", "", ""]
 
 let go = "circle"
+let circleScore = 0
+let crossScore = 0
+let roundText = document.querySelector(".round-text")
 infoDisplay.textContent = "Circle goes first"
+
+
+const buttonContainer = document.getElementById('buttonContainer');
+// Add a click event listener to the button container
+buttonContainer.addEventListener('click', button => {
+    // Check if the clicked element has the 'round' class
+    if (button.target.classList.contains('round')) {
+        const value = button.target.value;
+        roundText.textContent = `${value} rounds match`
+    }
+});
 
 function createBoard() {
     startCells.forEach((_cell, index) => {
@@ -28,41 +43,45 @@ function addGo(e) {
     checkScore()
 }
 
+
+// checking score of players to check who is winner
 function checkScore() {
-
-    const allSquares = document.querySelectorAll(".square")
-    console.log(allSquares[0])
-
+    const allSquares = document.querySelectorAll(".square");
     const winningCombos = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
-    ]
+    ];
+
+    let circleWins = false;
+    let crossWins = false;
 
     winningCombos.forEach(array => {
-        const circleWins = array.every(cell =>
-            allSquares[cell].firstChild?.classList.contains('circle'))
-
-        if (circleWins) {
-            infoDisplay.textContent = "Circle Wins"
-            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
-            return
+        if (array.every(cell => allSquares[cell].firstChild?.classList.contains('circle'))) {
+            circleWins = true;
+        } else if (array.every(cell => allSquares[cell].firstChild?.classList.contains('cross'))) {
+            crossWins = true;
         }
+    });
 
-    })
+    if (circleWins) {
+        infoDisplay.textContent = "Circle Wins";
+        circleScore += 1;
+        allSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+    } else if (crossWins) {
+        infoDisplay.textContent = "Cross Wins";
+        allSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+        crossScore++;
+    } else if ([...allSquares].every(square => square.firstChild)) {
+        infoDisplay.textContent = "It's a Draw!";
+    }
 
-    winningCombos.forEach(array => {
-        const crossWins = array.every(cell =>
-            allSquares[cell].firstChild?.classList.contains('cross'))
-
-        if (crossWins) {
-            infoDisplay.textContent = "Cross Wins"
-            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
-            return
-        }
-
-    })
+    winner.textContent = `circle ${circleScore} wins | cross ${crossScore} wins`;
 }
+
+
+
+// restart button functionality goes here
 
 const restartButton = document.getElementById("restart")
 restartButton.addEventListener("click", restartGame);
@@ -72,6 +91,8 @@ function restartGame() {
 }
 
 
+// reset button functionality goes here
+
 const resetButton = document.getElementById("reset")
 resetButton.addEventListener("click", resetGame)
 
@@ -80,7 +101,6 @@ function resetGame() {
     allSquares.forEach(square => square.innerHTML = "");
     go = "circle";
     infoDisplay.textContent = "Circle's turn";
-
     enableClicks();
 }
 
@@ -88,3 +108,5 @@ function enableClicks() {
     const allSquares = document.querySelectorAll(".square");
     allSquares.forEach(square => square.addEventListener("click", addGo));
 }
+
+
