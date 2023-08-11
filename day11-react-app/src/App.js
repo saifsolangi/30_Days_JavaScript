251eeb94c8f0e0f973e2abf353567bdfa04358ff
Saif Ali <saifsolangi2@gmail.com>
2023-08-11 09:48:52 +0500

    learning react so from day11 on ward will be working on this react

---
+++ day11-react-app/src/App.js
@@ -0,0 +1,15 @@
+import Video from './components/video';
+
+function App() {
+
+  return (
+    <>
+      <div>Hello</div>
+      <Video />
+
+    </>
+  )
+
+}
+
+export default App;
---
+++ day11-react-app/src/components/video.js
@@ -0,0 +1,13 @@
+function Video() {
+
+    return (
+        <div>
+            <img src="https://placebeard.it/640x360/any"></img>
+            <div>React JS Tutorial - Components</div>
+        </div>
+
+    )
+
+}
+
+export default Video;
